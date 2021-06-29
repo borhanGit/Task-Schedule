6b8fe71e7975800f18d4c016200f9843ea68f82b
mdborhan <mdborhan410@gmail.com>
2021-06-29 12:55:26 +0600

    task schedule in laravel first commit

---
+++ app/Console/Commands/DemoCron.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DemoCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'demo:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'It is the test purpose';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        \Log::info("Working fine");
+        $this->info('Demo:Cron Cummand Run successfully!');
+
+    }
+}
